75c87c029c0ee0ee037cf289b9220cde865415c2
Phu123z <166420975+Phu123z@users.noreply.github.com>
2025-04-14 23:07:09 +0700

    Create main_encrypt.py

---
+++ main_encrypt.py
@@ -0,0 +1,33 @@
+
+
+from des.file_io import read_file, write_file
+from des.encrypt import des_encrypt_block
+
+
+def str_to_bin(s):
+    return ''.join(f"{ord(c):08b}" for c in s)
+
+
+def pad_binary(bin_data):
+    while len(bin_data) % 64 != 0:
+        bin_data += "0"
+    return bin_data
+
+
+def main():
+    plaintext = read_file("des/input.txt")
+    key = read_file("des/key.txt")
+
+    bin_text = pad_binary(str_to_bin(plaintext))
+    bin_key = str_to_bin(key)[:64].ljust(64, "0")  
+    result = ""
+    for i in range(0, len(bin_text), 64):
+        block = bin_text[i:i + 64]
+        cipher_block = des_encrypt_block(block, bin_key)
+        result += cipher_block
+
+    write_file("des/output.txt", result)
+
+
+if __name__ == "__main__":
+    main()
