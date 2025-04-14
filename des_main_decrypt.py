d2a7e2f3fece1ba8645ea82b5d52d7d9c3a549d3
Phu123z <166420975+Phu123z@users.noreply.github.com>
2025-04-14 23:13:31 +0700

    Update des_main_decrypt.py

--- des_main_decrypt.py
+++ des_main_decrypt.py
@@ -1,27 +1,11 @@
-from des_file_io import read_file, write_file
-from des_decrypt import des_decrypt_block
-
-
-def bin_to_str(b):
-    chars = [b[i:i + 8] for i in range(0, len(b), 8)]
-    return ''.join(chr(int(c, 2)) for c in chars)
-
+from des_file_io import read_output, write_decrypted, read_key
+from des_decrypt import des_decrypt
 
 def main():
-    cipher = read_file("des/output.txt")
-    key = read_file("des/key.txt")
-
-    bin_key = ''.join(f"{ord(c):08b}" for c in key)[:64].ljust(64, "0")
-
-    result = ""
-    for i in range(0, len(cipher), 64):
-        block = cipher[i:i + 64]
-        plain_block = des_decrypt_block(block, bin_key)
-        result += plain_block
-
-    plain_text = bin_to_str(result).rstrip("\x00")
-    write_file("des/decrypted.txt", plain_text)
-
+    ciphertext = read_output("output.txt")
+    key = read_key("key.txt")
+    decrypted = des_decrypt(ciphertext, key)
+    write_decrypted("decrypted.txt", decrypted)
 
 if __name__ == "__main__":
     main()
